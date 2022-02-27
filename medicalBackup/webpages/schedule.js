5714fb3f6669ecc7d045434b9d3c9baccb21c8f2
Alexa Ponce <ajp3944@rit.edu>
2022-02-26 20:05:51 -0500

    made server from scratch, HTML pages work

---
+++ medicalBackup/webpages/schedule.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile('./schedule.html', {root: __dirname});
+})
+
+app.get('/user_info', (req, res) => {
+    res.sendFile('./user_info.html', {root: __dirname});
+})
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
